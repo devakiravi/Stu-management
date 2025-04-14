0742348c610dfdc3a185915f7dd8747c027bd41f
Devaki <devakiravi226@gmail.com>
2025-04-14 21:36:51 +0530

    Add files via upload

---
+++ StuManagement/src/test/java/com/student/StuManagement/StuManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.StuManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
